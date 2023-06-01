da735679c155930edca273b06f787f1cf664a941
uejo <rentarou1994@gmail.com>
2023-06-01 10:05:50 +0900

    Initial Commit

---
+++ BookSearchAppSwiftUI/BookSearchAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchAppSwiftUIApp.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
