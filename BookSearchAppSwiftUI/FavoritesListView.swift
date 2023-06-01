ed21b7dd7867556dc5c7f3a30143607d0feef13a
uejo <rentarou1994@gmail.com>
2023-06-01 23:13:00 +0900

    :lipstick: 仮のお気に入り一覧画面を作成

--- BookSearchAppSwiftUI/FavoritesListView.swift
+++ BookSearchAppSwiftUI/FavoritesListView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct FavoritesListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Text("Favorites List")
+                .font(.system(size: 38).bold())
+                .foregroundColor(.primary)
+            Text("お気に入りの本たち")
+                .font(.system(size: 20))
+                .foregroundColor(.secondary)
+        }
     }
 }
 
