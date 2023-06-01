cf7565e07c960887de43629382e033d69ba86c1f
uejo <rentarou1994@gmail.com>
2023-06-01 23:04:20 +0900

    :art: 本の一覧とお気に入りの一覧画面を作成

---
+++ BookSearchAppSwiftUI/BooksListView.swift
@@ -0,0 +1,20 @@
+//
+//  BooksListView.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/01.
+//
+
+import SwiftUI
+
+struct BooksListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BooksListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BooksListView()
+    }
+}
---
+++ BookSearchAppSwiftUI/FavoritesListView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListView.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/01.
+//
+
+import SwiftUI
+
+struct FavoritesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesListView()
+    }
+}
