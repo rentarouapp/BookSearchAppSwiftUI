8765f38484ff7c7c64afd9dfcabd792a8256424e
uejo <rentarou1994@gmail.com>
2023-06-02 14:57:14 +0900

    :art: テストのために3つ分の本のデータを内部に登録

---
+++ BookSearchAppSwiftUI/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/02.
+//
+
+import Foundation
+
+class Constants {
+    
+    public static let title_1: String = "スティーブ・ジョブズ捕物帖"
+    public static let title_2: String = "クック・クッキング"
+    public static let title_3: String = "かもめのジョナサン"
+    
+    public static let author_1: String = "Steve Jobs"
+    public static let author_2: String = "Tim Cook"
+    public static let author_3: String = "Jony Ive"
+    
+    public static let thumbUrl_1: String = "https://media.loom-app.com/gizmodo/dist/images/2018/07/31/180116_macbook_air_revealed_10years_ago.jpg?w=1280"
+    public static let thumbUrl_2: String = "https://www.applemust.com/wp-content/uploads/2018/04/Apple_Chicago_wrapup_Tim_Cook_welcomes_Keynote_audience_to_Lane_Tech_College_Preparatory_High_School_03272018.jpg"
+    public static let thumbUrl_3: String = "https://static.theceomagazine.net/wp-content/uploads/2019/07/10103645/jony-ive-apple-1100x733.jpg"
+    
+}
