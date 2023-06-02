ee89f086acb4b8e46ed0d194d8cdc991bb969df8
uejo <rentarou1994@gmail.com>
2023-06-02 23:21:46 +0900

    :lipstick: スクロールする詳細画面を作成

---
+++ BookSearchAppSwiftUI/BookDescriptionView.swift
@@ -0,0 +1,22 @@
+//
+//  BookDescriptionView.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/02.
+//
+
+import SwiftUI
+
+struct BookDescriptionView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct BookDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDescriptionView()
+    }
+}
