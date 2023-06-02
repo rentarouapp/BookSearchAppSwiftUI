d36b702198bcf99dcc4c7890e11bfa6f69ff4b58
uejo <rentarou1994@gmail.com>
2023-06-02 17:28:17 +0900

    :lipstick: 一覧のセルレイアウトをざっくりと行う

--- BookSearchAppSwiftUI/BookDetailView.swift
+++ BookSearchAppSwiftUI/BookDetailView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct BookDetailView: View {
+    var bookModel: BookModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            HStack(alignment: .center, spacing: 0) {
+                Spacer()
+                    .frame(maxWidth: 16, maxHeight: .infinity)
+                    .background(Color.blue)
+                if let url = URL(string: self.bookModel.thumbImageUrl) {
+                    AsyncImage(url: url) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: geometry.size.height, maxHeight: .infinity)
+                            .clipped() // 切り取る
+                    } placeholder: {
+                        Image(systemName: "star")
+                    }
+                    .frame(maxWidth: geometry.size.height, maxHeight: .infinity)
+                    .background(Color.red)
+                }
+                VStack {
+                    Text(self.bookModel.bookTitle)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    Text(self.bookModel.authorName)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                Spacer()
+                    .frame(maxWidth: 16, maxHeight: .infinity)
+                    .background(Color.blue)
+            }
+            .frame(width: .infinity, height: 120)
+        }
+        .frame(width: .infinity, height: 120)
     }
 }
 
 struct BookDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        BookDetailView()
+        Group {
+            BookDetailView(bookModel: BookModel(bookTitle: Constants.title_1, authorName: Constants.author_1, thumbImageUrl: Constants.thumbUrl_1))
+            BookDetailView(bookModel: BookModel(bookTitle: Constants.title_2, authorName: Constants.author_2, thumbImageUrl: Constants.thumbUrl_2))
+        }.previewLayout(.sizeThatFits)
     }
 }
