e27f2c846ee436613adfd4170de21d97928936ef
uejo <rentarou1994@gmail.com>
2023-06-05 21:34:11 +0900

    :pencil2: 定数クラスの編集

--- BookSearchAppSwiftUI/Constants.swift
+++ BookSearchAppSwiftUI/Constants.swift
@@ -15,8 +15,19 @@ class Constants {
     public static let author: String = "著者"
     // 発刊年月日
     public static let releasedDate: String = "発刊年月日"
+    // 本の説明
+    public static let bookDescription: String = "本の説明"
     // 画像なし
     public static let noImageIcon: String = "no_image"
+    // お気に入り星マーク
+    public static let star: String = "star"
+    public static let starFill: String = "star.fill"
+    // Safariマーク
+    public static let safari: String = "safari"
+    
+    // お気に入りに追加
+    public static let addFavorite: String = "お気に入りに追加"
+    public static let openBrowser: String = "ブラウザで開く"
     
     // テスト
     public static let title_1: String = "スティーブ・ジョブズ捕物帖"
@@ -30,5 +41,7 @@ class Constants {
     public static let thumbUrl_1: String = "https://media.loom-app.com/gizmodo/dist/images/2018/07/31/180116_macbook_air_revealed_10years_ago.jpg?w=1280"
     public static let thumbUrl_2: String = "https://www.applemust.com/wp-content/uploads/2018/04/Apple_Chicago_wrapup_Tim_Cook_welcomes_Keynote_audience_to_Lane_Tech_College_Preparatory_High_School_03272018.jpg"
     public static let thumbUrl_3: String = "https://static.theceomagazine.net/wp-content/uploads/2019/07/10103645/jony-ive-apple-1100x733.jpg"
+    
+    public static let descriptionText: String = "あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああLorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
     
 }
