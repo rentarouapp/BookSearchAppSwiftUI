b242c7642946c0bdc3bb00a9dca7d7d416c8bbcd
uejo <rentarou1994@gmail.com>
2023-06-07 22:56:51 +0900

    :art: Mockの作成

--- BookSearchAppSwiftUI/Constants.swift
+++ BookSearchAppSwiftUI/Constants.swift
@@ -9,6 +9,12 @@ import Foundation
 
 class Constants {
     
+    enum MockPerson {
+        case steve_jobs
+        case tim_cook
+        case jony_ive
+    }
+    
     // タイトル
     public static let title: String = "タイトル"
     // 著者
@@ -43,5 +49,47 @@ class Constants {
     public static let thumbUrl_3: String = "https://static.theceomagazine.net/wp-content/uploads/2019/07/10103645/jony-ive-apple-1100x733.jpg"
     
     public static let descriptionText: String = "あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああLorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+    
+    public static var infoLink: String = "https://www.apple.com/apple-vision-pro/?&cid=wwa-jp-kwgo-avalanche-slid--Brand-Avalanche-Announce&mnid=s7F2l6cZ6-dc_mtid_2092567642642_pcrid_661168735388_pgrid_150513434832_pexid__&mtid=2092567642642&aosid=p238"
+    
+    // Mock
+    static func getSampleBookItem(mockPerson: MockPerson) -> BookItem {
+        
+        var mockId: String = ""
+        var mockTitle: String = ""
+        var mockAuthros: [String] = []
+        var mockImageThumb: String = ""
+        
+        switch mockPerson {
+        case .steve_jobs:
+            mockId = "1"
+            mockTitle = Constants.title_1
+            mockAuthros = [Constants.author_1]
+            mockImageThumb = Constants.thumbUrl_1
+            
+        case .tim_cook:
+            mockId = "2"
+            mockTitle = Constants.title_2
+            mockAuthros = [Constants.author_2]
+            mockImageThumb = Constants.thumbUrl_2
+            
+        case .jony_ive:
+            mockId = "3"
+            mockTitle = Constants.title_3
+            mockAuthros = [Constants.author_3]
+            mockImageThumb = Constants.thumbUrl_3
+        }
+        
+        
+        let imageLinks: ImageLinks = ImageLinks(thumbnail: mockImageThumb,
+                                                smallThumbnail: mockImageThumb)
+        let volumeInfo: VolumeInfo = VolumeInfo(title: mockTitle,
+                                                authors: mockAuthros,
+                                                imageLinks: imageLinks,
+                                                infoLink: Constants.infoLink,
+                                                publishedDate: "2023-06-07",
+                                                description: Constants.descriptionText)
+        return BookItem(id: mockId, volumeInfo: volumeInfo)
+    }
     
 }
