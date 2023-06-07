056ede7c2fa639436f52d2002b2c5a5d86888d9e
uejo <rentarou1994@gmail.com>
2023-06-07 19:42:47 +0900

    :art: API通信クラスと結果をパースするモデルクラスを作成

---
+++ BookSearchAppSwiftUI/BookListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  BookListViewModel.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/07.
+//
+// API通信
+
+import Foundation
+import Alamofire
+import Combine
+
+final class BookListViewModel: ObservableObject {
+    
+    
+    
+}
+
---
+++ BookSearchAppSwiftUI/BookSearchRepositories.swift
@@ -0,0 +1,9 @@
+//
+//  BookSearchRepositories.swift
+//  BookSearchAppSwiftUI
+//
+//  Created by 上條蓮太朗 on 2023/06/07.
+//
+// モデル
+
+import Foundation
