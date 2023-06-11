361467ae738e745a3324c8d92bdd7bd20b385408
uejo <rentarou1994@gmail.com>
2023-06-11 16:49:18 +0900

    :art: modifierを呼び出すExtensionを追加

--- BookSearchAppSwiftUI/Extensions.swift
+++ BookSearchAppSwiftUI/Extensions.swift
@@ -5,4 +5,11 @@
 //  Created by 上條蓮太朗 on 2023/06/11.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    // アラート表示のカスタムボタン
+    func customAlert(for alertEntity: Binding<AlertEntity>) -> some View {
+        modifier(CustomAlertView(alertEntity: alertEntity))
+    }
+}
